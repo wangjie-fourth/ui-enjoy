196214893e39dba5418c65e74457109e91e6f2f5
wangjie <wangjie_fourth@163.com>
2021-06-23 22:53:15 +0800

    [k] 项目模块化
    1. 使用parcel单独转译.vue文件
    2. scss写法，使用&表示当前元素为父元素
    
    note:
    .vue文件只是将原来的纯js写法做了一些封装，理解原本状态

---
+++ src/button.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <button class="g-button">按钮</button>
+
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+    .g-button {
+        height: var(--button-height);
+        padding: 0 1em;
+        font: inherit;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        font-size: var(--font-size);
+
+        &:hover{
+            border-color: var(--border-color-hover);
+        }
+        &:active{
+            background-color: var(--button-bg);
+        }
+        &:focus{
+            outline: none;
+        }
+    }
+</style>
