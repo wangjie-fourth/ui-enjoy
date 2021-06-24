015f3e9fd137e2663be4b95f213dc5fd22bd783d
wangjie <wangjie_fourth@163.com>
2021-06-24 22:21:23 +0800

    [k] button样式

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-            <g-icon v-if="icon" :name="icon"></g-icon>
+            <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
             <div class="content">
                 <slot></slot>
             </div>
