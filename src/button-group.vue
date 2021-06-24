41de50b37d21a7a5f3f5c555d1dc86582fcb4844
wangjie <wangjie_fourth@163.com>
2021-06-24 23:02:00 +0800

    [k] g-button-group

---
+++ src/button-group.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "button-group",
+        mounted() {
+            for (let node of this.$el.children) {
+                if (node.nodeName.toLowerCase() !== 'button') {
+                    console.warn('g-button-group 的子元素应该全是 g-button')
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .g-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+
+        > .g-button {
+            border-radius: 0;
+
+            margin-left: -1px;
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+
+            &:last-child {
+                border-top-right-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+        }
+    }
+</style>
